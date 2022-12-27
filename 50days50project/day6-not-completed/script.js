f355e657a878fb67734c88d00602acfa3911b716
Clophy <barangenez@hotmail.com>
2022-12-27 16:03:40 +0300

    Update script.js

--- 50days50project/day6-not-completed/script.js
+++ 50days50project/day6-not-completed/script.js
@@ -1,6 +1,6 @@
 const BOX = document.querySelectorAll(".box")
 
-
+/*BURADA SCROLL AŞAĞIYA DOĞRU GİTTİĞİNDE İÇERİKLER LİSTELENECEK YUKARIYA DOĞRU GİTTİĞİNDE İÇERİKLER KAYBOLACAK */
 window.addEventListener("scroll", e=>{
     BOX.forEach(item => item.classList.add("show"))
 })
